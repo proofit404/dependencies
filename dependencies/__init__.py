5e6fba52e75228458598f4e250453c369e854c75
Artem Malyshev <proofit404@gmail.com>
2016-01-21 22:21:38 +0300

    Correct copyright year.

--- dependencies/__init__.py
+++ dependencies/__init__.py
@@ -4,7 +4,7 @@
 
     Dependency Injection for Humans.
 
-    :copyright: (c) 2015 by Artem Malyshev.
+    :copyright: (c) 2016 by Artem Malyshev.
     :license: LGPL-3, see LICENSE for more details.
 """
 
