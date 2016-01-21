ebe78a0eb7ce768102567a0454e9b68583a13f7e
Artem Malyshev <proofit404@gmail.com>
2016-01-21 20:01:19 +0300

    Add dependencies package.

---
+++ dependencies/__init__.py
@@ -0,0 +1,9 @@
+"""
+    dependencies
+    ~~~~~~~~~~~~
+
+    Dependency injection for Humans.
+
+    :copyright: (c) 2015 by Artem Malyshev.
+    :license: LGPL-3, see LICENSE for more details.
+"""
