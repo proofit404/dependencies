b3a15162d441049a33098b7a01cf7e08e7bc824a
Artem Malyshev <proofit404@gmail.com>
2016-01-21 20:22:30 +0300

    Spelling.

--- dependencies/__init__.py
+++ dependencies/__init__.py
@@ -2,7 +2,7 @@
     dependencies
     ~~~~~~~~~~~~
 
-    Dependency injection for Humans.
+    Dependency Injection for Humans.
 
     :copyright: (c) 2015 by Artem Malyshev.
     :license: LGPL-3, see LICENSE for more details.
