46f9df524527ff80db677bb7df1c3104097db1c6
Artem Malyshev <proofit404@gmail.com>
2016-01-21 22:08:15 +0300

    Add constructor-based DI.

--- dependencies/__init__.py
+++ dependencies/__init__.py
@@ -8,10 +8,34 @@
     :license: LGPL-3, see LICENSE for more details.
 """
 
+import six
+
 __all__ = ['Injectable', 'Injector']
 
 
-class Injectable:
+class InjectableBase(type):
+
+    def __new__(cls, name, bases, namespace):
+
+        new = super(InjectableBase, cls).__new__
+
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+
+        def __getattr__(self, name):
+            try:
+                return self.kwargs[name]
+            except KeyError:
+                return getattr(super(), name)
+
+        namespace['__init__'] = __init__
+        namespace['__getattr__'] = __getattr__
+
+        return new(cls, name, bases, namespace)
+
+
+@six.add_metaclass(InjectableBase)
+class Injectable(object):
     """Dependency Injection target.
 
     Classes inherited from this class may use constructor-based
@@ -23,7 +47,7 @@ class Injectable:
     pass
 
 
-class Injector:
+class Injector(object):
     """Default dependencies specification DSL.
 
     Classes inherited from this class may specify default dependencies
--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -4,4 +4,9 @@ from dependencies import Injectable, Injector
 def test_constructor_based_di():
     """Classed inherited from `Injectable` allows constructor-based DI."""
 
-    pass
+    class Foo(Injectable):
+        def apply(self, command):
+            return self.modifier(command)
+
+    foo = Foo(modifier=lambda x: x.upper())
+    assert foo.apply('ls') == 'LS'
