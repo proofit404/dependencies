9991655fbf62640b4d64ab25ea097aa64cc5b7d9
Artem Malyshev <proofit404@gmail.com>
2016-10-25 15:54:53 +0300

    Add TODO notes.

--- dependencies/__init__.py
+++ dependencies/__init__.py
@@ -13,3 +13,9 @@ from .proxies import attribute, item
 from .exceptions import DependencyError
 
 __all__ = ['Injector', 'attribute', 'item', 'DependencyError']
+
+# TODO: allow to use `use` decorator without explicit attributes
+#
+# TODO: write documentation for proxies module
+#
+# TODO: write documentation for new `use` decorator
