236816340df75e1ef4fc3f6ee4540a02c44fc279
Artem Malyshev <proofit404@gmail.com>
2017-12-04 09:09:24 +0300

    Support argument name in the CodeCollector helper.

--- tests/helpers/helpers.py
+++ tests/helpers/helpers.py
@@ -5,8 +5,9 @@ import pytest
 
 class CodeCollector(object):
 
-    def __init__(self):
+    def __init__(self, name='code'):
 
+        self.name = name
         self.collected = []
 
     def __call__(self, f):
@@ -20,4 +21,4 @@ class CodeCollector(object):
 
     def parametrize(self, test_func):
 
-        return pytest.mark.parametrize('code', self, ids=getdoc)(test_func)
+        return pytest.mark.parametrize(self.name, self, ids=getdoc)(test_func)
