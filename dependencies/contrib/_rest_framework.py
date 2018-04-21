65c784ec3c3e6a9e370b8266e3a8aedf69ab02d9
Artem Malyshev <proofit404@gmail.com>
2018-04-21 22:22:27 +0300

    Create django rest framework contrib layout.

---
+++ dependencies/contrib/_rest_framework.py
@@ -0,0 +1,4 @@
+def api_view(injector):
+    """Create DRF class-based API view from injector class."""
+
+    pass
---
+++ dependencies/contrib/rest_framework.py
@@ -0,0 +1,13 @@
+"""
+dependencies.contrib.rest_framework
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module implements injectable Django Rest Framework API view.
+
+:copyright: (c) 2018 by Artem Malyshev.
+:license: BSD, see LICENSE for more details.
+"""
+
+from ._rest_framework import api_view
+
+__all__ = ["api_view"]
