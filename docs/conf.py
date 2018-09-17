5e318d9a5ba8bb070d547cbd635dc8614423c950
Artem Malyshev <proofit404@gmail.com>
2018-09-17 15:17:51 +0300

    Do not show powered by info.

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ html_sidebars = {
 }
 
 html_theme_options = {
+    "show_powered_by": False,
     "show_related": True,
     "description": "Dependency Injection for Humans.  It provides a simple low-impact implementation of an IoC container and resolution support for your classes.",
     "github_user": "dry-python",
