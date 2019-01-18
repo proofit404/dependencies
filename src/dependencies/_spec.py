ef9536da211ec00f116ae599de29427a18961f9c
Artem Malyshev <proofit404@gmail.com>
2019-01-18 17:20:41 +0300

    Rewrite init spec to be compatible with functions.

--- src/dependencies/_spec.py
+++ src/dependencies/_spec.py
@@ -21,34 +21,38 @@ def use_object_init(cls):
             return False
 
 
+def make_init_spec(dependency):
+
+    name = constructor_name(dependency)
+    args, have_defaults = make_func_spec(dependency.__init__, name)
+    return args[1:], have_defaults
+
+
 try:
     inspect.signature
 except AttributeError:
 
-    def make_init_spec(dependency):
+    def make_func_spec(func, funcname):
 
-        argspec = inspect.getargspec(dependency.__init__)
-        args, varargs, kwargs, defaults = argspec
-        check_varargs(constructor_name(dependency), varargs, kwargs)
+        args, varargs, kwargs, defaults = inspect.getargspec(func)
+        check_varargs(funcname, varargs, kwargs)
         if defaults is not None:
             check_cls_arguments(args, defaults)
             have_defaults = len(args) - len(defaults)
         else:
             have_defaults = len(args)
-        spec = args[1:], have_defaults
-        return spec
+        return args, have_defaults
 
 
 else:
 
-    def make_init_spec(dependency):
+    def make_func_spec(func, funcname):
 
-        signature = inspect.signature(dependency.__init__)
+        signature = inspect.signature(func)
         parameters = iter(signature.parameters.items())
-        next(parameters)
         args, defaults = [], []
         varargs = kwargs = None
-        have_defaults = 1
+        have_defaults = 0
         for name, param in parameters:
             args.append(name)
             if param.default is not param.empty:
@@ -59,7 +63,7 @@ else:
                 varargs = True
             if param.kind is param.VAR_KEYWORD:
                 kwargs = True
-        check_varargs(constructor_name(dependency), varargs, kwargs)
+        check_varargs(funcname, varargs, kwargs)
         if defaults:
             check_cls_arguments(args, defaults)
         return args, have_defaults
@@ -72,7 +76,6 @@ def check_cls_arguments(argnames, defaults):
         is_class = inspect.isclass(value)
         if expect_class and not is_class:
             raise DependencyError("{0!r} default value should be a class".format(name))
-
         if not expect_class and is_class:
             raise DependencyError(
                 "{0!r} argument can not have class as its default value".format(name)
