39eed36a2a505e583e2b2f2e39ea63060bb3192d
Artem Malyshev <proofit404@gmail.com>
2019-05-18 15:18:28 +0300

    Setup mypy type checker.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-# Do not run actual tests in linter environments.
-
-
 def pytest_collection_modifyitems(session, config, items):
-    for linter in ["flake8", "black", "isort"]:
+    """
+    Do not run actual tests in linter environments.
+    """
+    for linter in ["flake8", "mypy", "black", "isort"]:
         try:
             if config.getoption("--" + linter):
                 items[:] = [item for item in items if item.get_closest_marker(linter)]
