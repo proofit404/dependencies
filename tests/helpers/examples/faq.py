3a79ba813f063e837273edc26b392bcace170dec
Artem Malyshev <proofit404@gmail.com>
2019-08-10 03:07:32 +0300

    Rewrite code examples in the "FAQ" chapter into doctest.

---
+++ tests/helpers/examples/faq.py
@@ -0,0 +1,22 @@
+class AbstractNotificationService:
+    pass
+
+
+class AbstractPaymentService:
+    pass
+
+
+class TypedInjector:
+    def register(self, arg):
+        pass
+
+    def build(self, arg):
+        return arg(PaypalService(), SMSService())
+
+
+class SMSService(AbstractNotificationService):
+    pass
+
+
+class PaypalService(AbstractPaymentService):
+    pass
--- tests/helpers/mddoctest.py
+++ tests/helpers/mddoctest.py
@@ -2,12 +2,19 @@ import sys
 from doctest import testfile
 from glob import glob
 
+import responses
 from django.apps import apps
 from django.conf import settings
 
 
 def main():
     apps.populate(settings.INSTALLED_APPS)
+    responses.add(
+        responses.GET,
+        "http://api.com/users/1/",
+        json={"id": 1, "name": "John", "surname": "Doe"},
+    )
+    responses.start()
     markdown_files = glob("**/*.md", recursive=True)
     exit_code = 0
     for markdown_file in markdown_files:
