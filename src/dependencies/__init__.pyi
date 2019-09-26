8f1bbda288a82bd293467c1a95309fb250393ab2
Artem Malyshev <proofit404@gmail.com>
2019-09-26 23:38:14 +0300

    Move stub files to the proper package.

--- src/dependencies/__init__.pyi
+++
@@ -1,5 +0,0 @@
-from ._injector import Injector as Injector  # noqa
-from ._operation import Operation as operation  # noqa
-from ._package import Package as Package  # noqa
-from ._this import this as this  # noqa
-from ._value import Value as value  # noqa
