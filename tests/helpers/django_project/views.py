23039d345088bbfaa394d9b78330a9005498ad5c
Artem Malyshev <proofit404@gmail.com>
2019-12-11 03:33:45 +0300

    Ignore partially covered template view.

--- tests/helpers/django_project/views.py
+++ tests/helpers/django_project/views.py
@@ -90,7 +90,10 @@ class DynamicQuestionTemplateView(Injector):
     content_type = "text/html"
 
     @value
-    def extra_context(user):
+    # Old versions of Django does not access this attribute while
+    # processing template view.  So not all Tox environment will
+    # report this function called.
+    def extra_context(user):  # pragma: no branch
 
         assert isinstance(user, AnonymousUser)
         return {"extra_var": "extra-var"}
