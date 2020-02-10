f0ec9f33366f81934807be3f2ec8adffe30a8198
Artem Malyshev <proofit404@gmail.com>
2020-02-10 16:45:00 +0300

    ci: setup semantic-release tool

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  repositoryUrl: "https://github.com/dry-python/dependencies",
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/changelog.md"
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "poetry version ${nextRelease.version} && poetry build",
+        publishCmd: "poetry publish"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/changelog.md", "pyproject.toml"]
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [{ path: "dist/*.whl" }, { path: "dist/*.tar.gz" }]
+      }
+    ]
+  ]
+};
