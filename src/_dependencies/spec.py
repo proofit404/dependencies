9f6ab1ed445cbb11c1ecb0a3e5e047fd1ac221a6
Artem Malyshev <proofit404@gmail.com>
2021-07-05 09:17:57 +0300

    refactor: validate spec args #479

--- src/_dependencies/spec.py
+++ src/_dependencies/spec.py
@@ -3,6 +3,9 @@ from _dependencies.exceptions import DependencyError
 
 class _Spec:
     def __init__(self, factory, args, required, optional, resolve):
+        _validate_factory(factory)
+        _validate_args(args, required, optional)
+        _validate_resolve(resolve)
         self.factory = factory
         self.args = args
         self.required = required
@@ -14,3 +17,18 @@ class _Spec:
         if kind is not None:
             message = f"{kind} dependencies could only be used to instantiate classes"
             raise DependencyError(message)
+
+
+def _validate_factory(factory):
+    if not callable(factory):
+        raise RuntimeError
+
+
+def _validate_args(args, required, optional):
+    if not (args.keys() == required ^ optional and not required & optional):
+        raise RuntimeError
+
+
+def _validate_resolve(resolve):
+    if not callable(resolve):
+        raise RuntimeError
