58e213fe69ee4927b75fdb7ec6e3010c101bdf59
Artem Malyshev <proofit404@gmail.com>
2021-07-07 09:11:21 +0300

    feat: deny to resolve value object directly #473
    BREAKING CHANGE: value objects could only be used to evaluate
    arguments of classes. Attribute access of Injector subclass
    which result in value object would raise an error.

--- src/_dependencies/spec.py
+++ src/_dependencies/spec.py
@@ -1,7 +1,8 @@
 class _Spec:
-    def __init__(self, factory, args, required, optional, resolvable=True):
+    def __init__(self, factory, args, required, optional, kind=None, resolvable=True):
         self.factory = factory
         self.args = args
         self.required = required
         self.optional = optional
+        self.kind = kind
         self.resolvable = resolvable
