340694669c0abff951a1e5c197ea0b10657299ce
Artem Malyshev <proofit404@gmail.com>
2021-07-17 14:58:48 +0300

    feat: deny classes to depend on nested injectors #479
    BREAKING CHANGE: Injector won't pass nested injectors to the constructor
    of the class. Use `this` objects to access inner attributes of nested
    injector and pass them to the constructor arguments.

--- src/_dependencies/scope.py
+++ src/_dependencies/scope.py
@@ -1,11 +1,15 @@
 from _dependencies.resolve import _Resolver
 
 
+class _IsScope:
+    pass
+
+
 class _Scope:
     def __new__(cls, graph):
         def getattr_method(self, attrname):
             return _Resolver(graph, cache, attrname).resolve()
 
-        instance = type(cls.__name__, (), {"__getattr__": getattr_method})()
+        instance = type(cls.__name__, (_IsScope,), {"__getattr__": getattr_method})()
         cache = {"__self__": instance}
         return instance
