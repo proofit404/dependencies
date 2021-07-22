612b522e48e36a4278cea7dd5a9289d66e70a217
Artem Malyshev <proofit404@gmail.com>
2021-07-22 17:48:40 +0300

    feat: implement sticky scopes #311
    All objects instantiated during sticky scope life-time should be instantiated once.
    That way we would be able to access inner structures of high-level instancies which
    may protect their inner state (which is a good thing).

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -48,3 +48,44 @@ def _a1Jv3seOJb6U(Result):
         cache=this.Nested.cache,
         Nested=Injector(graph=1, cache=2),
     ).result
+
+
+sticky_scope = CodeCollector()
+
+
+@sticky_scope.parametrize
+def test_sticky_scope(code):
+    """Check sticky scope implementation.
+
+    All objects instantiated during sticky scope life-time should be instantiated once.
+    That way we would be able to access inner structures of high-level instancies which
+    may protect their inner state (which is a good thing).
+
+    """
+
+    class App:
+        def __init__(self, db):
+            self.db = db
+
+    class DB:
+        pass
+
+    Container = code(App, DB)
+    with Container as container:
+        app = container.app
+        db = container.db
+    assert app.db is db
+
+
+@sticky_scope
+def _wz3C9jpJjT4A(App, DB):
+    class Container(Injector):
+        app = App
+        db = DB
+
+    return Container
+
+
+@sticky_scope
+def _a2w333ZI7L0a(App, DB):
+    return Injector(app=App, db=DB)
