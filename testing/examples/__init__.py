ab398faae271f89ed59f192ac23217de3d2650ae
Artem Malyshev <proofit404@gmail.com>
2022-10-21 21:12:17 +0300

    test: pass package object as shield argument #560

--- testing/examples/__init__.py
+++ testing/examples/__init__.py
@@ -1,1 +1,3 @@
 init_variable = 1
+a = 1
+b = 2
--- tests/test_objects_shield.py
+++ tests/test_objects_shield.py
@@ -1,19 +1,22 @@
 """Tests related to Shield object."""
 from dependencies import Injector
+from dependencies import Package
 from dependencies import shield
 from dependencies import this
 
 
-def test_pass_args(e):
+def test_pass_args(e, expect):
     """Pass positional arguments."""
 
     class Container(Injector):
         result = shield(e.StarArgs, 1, 2)
 
-    assert Container.result.args == (1, 2)
+    @expect(Container)
+    def to_be(it):
+        assert it.result.args == (1, 2)
 
 
-def test_pass_args_this(e):
+def test_pass_args_this(e, expect):
     """Pass resolved this object as positional arguments."""
 
     class Container(Injector):
@@ -21,4 +24,18 @@ def test_pass_args_this(e):
         a = 1
         b = 2
 
-    assert Container.result.args == (1, 2)
+    @expect(Container)
+    def to_be(it):
+        assert it.result.args == (1, 2)
+
+
+def test_pass_args_package(e, expect):
+    """Pass resolved package object as positional arguments."""
+    examples = Package("examples")
+
+    class Container(Injector):
+        result = shield(e.StarArgs, examples.a, examples.b)
+
+    @expect(Container)
+    def to_be(it):
+        assert it.result.args == (1, 2)
